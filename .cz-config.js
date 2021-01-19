7a82b7a8df4ab4e3ef7d01c2b3a33a1bcb78a03d
renjianhua <renjianhua@titanmatrix.com>
2021-01-19 12:45:20 +0800

    build: 增加ide支持

--- .cz-config.js
+++ .cz-config.js
@@ -1,35 +1,11 @@
-'use strict'
+const idecommit = require('./conventionalcommit.json')
 
 module.exports = {
-  types: [
-    { value: '特性', name: '特性:    一个新的特性' },
-    { value: '修复', name: '修复:    修复一个Bug' },
-    { value: '文档', name: '文档:    变更的只有文档' },
-    { value: '格式', name: '格式:    空格, 分号等格式修复' },
-    { value: '重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-    { value: '性能', name: '性能:    提升性能' },
-    { value: '测试', name: '测试:    添加一个测试' },
-    { value: '工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-    { value: '回滚', name: '回滚:    代码回退' },
-  ],
-  scopes: [{ name: '模块1' }, { name: '模块2' }, { name: '模块3' }, { name: '模块4' }],
-
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*
-  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
-  // override the messages, defaults are as follows
+  types: Object.keys(idecommit.types).map(k => ({ value: k, name: idecommit.types[k].description })),
+  scopes: Object.keys(idecommit.commonScopes).map(k => ({ name: k })),
   messages: {
     type: '选择一种你的提交类型:',
     scope: '选择一个模块 (可选):',
-    // used if allowCustomScopes is true
     customScope: 'Denote the SCOPE of this change:',
     subject: '短说明:\n',
     body: '长说明，使用"|"换行(可选)：\n',
@@ -39,8 +15,6 @@ module.exports = {
   },
 
   allowCustomScopes: true,
-  allowBreakingChanges: ['特性', '修复'],
-
-  // limit subject length
+  allowBreakingChanges: idecommit.footerTypes.map(k => k.name),
   subjectLimit: 100,
 }
