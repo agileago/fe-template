aa60c2ce60d712a334a47938cfad858c3d7723e4
renjianhua <renjianhua@titanmatrix.com>
2021-04-08 12:47:33 +0800

    样式: fix

--- src/auth/login/login.service.ts
+++ src/auth/login/login.service.ts
@@ -1,15 +1,6 @@
 import { reactive, InjectionKey, inject } from 'vue'
 import { useForm } from '@ant-design-vue/use'
 import { message } from 'ant-design-vue'
-import * as api from '../../api/define'
-
-api.postSeriesSortId({
-  path: { id: 1 },
-  body: { sort: [{ seriesId: 1, ptypeId: 1, showOrder: 1 }] },
-  query: {
-    name: 'aaa',
-  },
-})
 
 export default class LoginService {
   static token: InjectionKey<LoginService> = Symbol()
