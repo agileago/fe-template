3446deb307d71479f907049ff124190ea607d1de
agileago <agileago@gmail.com>
2021-12-19 10:31:13 +0800

    工具: 更新版本

--- src/module/count/count.sercice.ts
+++ src/module/count/count.sercice.ts
@@ -1,9 +1,9 @@
-import { Ref, VueService } from 'vue3-oop'
+import { Track, VueService } from 'vue3-oop'
 import { Injectable } from 'injection-js'
 
 @Injectable()
 export class CountSercice extends VueService {
-  @Ref() count = 1
+  @Track() count = 1
   add() {
     this.count++
   }
--- vite.config.ts
+++ vite.config.ts
@@ -26,10 +26,6 @@ export default defineConfig(({ command, mode }) => {
         {
           libName: 'ant-design-vue',
           style(name) {
-            if (/popconfirm/.test(name)) {
-              // support multiple style file path to import
-              return ['ant-design-vue/es/button/style/index.css', 'ant-design-vue/es/popover/style/index.css']
-            }
             return `ant-design-vue/es/${name}/style/index.css`
           },
         },
